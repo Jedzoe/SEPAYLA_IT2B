666041d84602c3ec2b6d1811d054f06c15c77a4c
jeedd <jedsepayla213@gmail.com>
2026-08-24 10:26:30 +0800

    Activity1

---
+++ src/Activity1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Activity1 {
+Scanner sc = new Scanner(System.in);
+
+    public void userGreetings(){
+        System.out.print("Enter Name: ");
+        String name = sc.next();
+        System.out.print("Enter Birth year: ");
+        int byear = sc.nextInt();
+        System.out.print("Enter gender: ");
+        String gender = sc.next();
+
+        System.out.println("========================================================");
+        System.out.println("Hello "+name+" ! Welcome to Java");
+        System.out.println("You were born in "+byear+" and your gender is "+gender);
+        System.out.println("========================================================");
+    }
+
+}
